f5b2b4e1c2e1a0a66d568473a1a4ec28150c5e5d
12Timoshka <115871394+12Timoshka@users.noreply.github.com>
2022-10-23 15:26:12 +0300

    Add files via upload

---
+++ Exercise_4.js
@@ -0,0 +1,29 @@
+<DOCTYPE! html>
+    <html lang="en">
+        
+        <head>
+            <title>Finder</title>
+            <meta charset="utf-8">
+
+            <style>
+                .container {
+                    display: flex;
+                    align-items: center;
+                    justify-content: center;
+                    width: 100%;
+                    height: 100%;
+                }
+                .box {
+                    margin: 5px;
+                }
+            </style>
+        </head>
+
+        <body>
+            <div class="container">
+                <div class="box"><input type="text"></div>
+                <div class="box"><input type="button"></div>
+            </div>    
+        </body>
+
+    </html>
